3b92c7af540a18d1184f25980ef5d912787b7949
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-07-29 11:47:30 +0200

    init

---
+++ src/main/java/com/eltayeb/e_learn/ELearnApplication.java
@@ -0,0 +1,13 @@
+package com.eltayeb.e_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearnApplication.class, args);
+	}
+
+}
